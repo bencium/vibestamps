d38481a075d2a9de44055f2198f65a4a08b0ee53
Ray Fernando <104695450+RayFernando1337@users.noreply.github.com>
2025-03-31 13:27:30 -0700

    larger file size for my longer livestreams

--- lib/constants.ts
+++ lib/constants.ts
@@ -2,5 +2,5 @@
  * Application-wide constants
  */
 
-// Max file size in bytes (230 KB)
-export const MAX_FILE_SIZE = 230 * 1024;
+// Max file size in bytes (420 KB)
+export const MAX_FILE_SIZE = 420 * 1024;
